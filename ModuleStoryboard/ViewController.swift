55f8326329a22750d3dd710722e7b35452a508f0
Vampirius1 <111462054+Vampirius1@users.noreply.github.com>
2022-10-08 13:45:20 +0300

    Initial Commit

---
+++ ModuleStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ModuleStoryboard
+//
+//  Created by Владислав on 08.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
